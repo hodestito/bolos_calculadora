4556956ce11e7f9a1c15ecdec276ec56ace052e4
Hugo <hodestito@gmail.com>
2020-04-12 15:34:46 -0300

    Configuracao da porta

--- main.py
+++ main.py
@@ -88,4 +88,5 @@ def not_found(error=None):
 
 if __name__ == "__main__":
     #p = Popen(['python server.py'], shell=True, stdout=PIPE, stderr=PIPE)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
